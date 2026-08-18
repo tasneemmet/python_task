dae3ca1c49caab65f26542b3dc8721283f44275d
Tasneem Amr <ta959001@gmail.com>
2026-08-18 07:28:58 +0300

    Add user profile card input and display

---
+++ tasks/task5.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+job = input("Enter your job: ")
+city = input("Enter your city: ")
+hobby = input("Enter your hobby: ")
+
+print("\n--- Profile Card ---")
+print(f"{name} from {city} has {age} yo and job: {job} and hobby {hobby}")
